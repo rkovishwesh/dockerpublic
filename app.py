c7128e4fb194bec59ce133756082e6232df18e64
root <root@ip-172-31-3-244.ap-south-1.compute.internal>
2020-06-21 10:14:17 +0000

    this my changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 @app.route("/")
 def hello():
-  return "welcome to jenkins changes!"
+  return render_template('login.html')
 if __name__== "__main__":
   app.run(host="0.0.0.0", port=int("5000"), debug=True)
